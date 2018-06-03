888d2b2f77749a15d4c9c9d707228a31b62b19bf
sparkecho <echozhz@126.com>
2018-06-03 22:39:31 +0800

    Init commit

---
+++ package.lisp
@@ -0,0 +1,17 @@
+;;;; package.lisp
+
+(defpackage #:dip
+  (:use #:cl)
+  (:export #:imread
+           #:imwrite
+           #:imshow
+           #:height
+           #:width
+           #:rows
+           #:cols
+           #:channels
+           #:array2d->3d
+           #:array3d->2d
+           #:convert
+           #:copy
+           #:flip))
