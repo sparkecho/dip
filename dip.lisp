9129065cc89f8b0cd75d00a3935c0b508a6bdd71
sparkecho <echozhz@126.com>
2018-06-04 01:46:58 +0800

    Basically finish imshow sdl2 backend.

--- dip.lisp
+++ dip.lisp
@@ -240,7 +240,19 @@ void main()
 
 ;;;; imshow with sdl2 as backend
 (defun imshow-sdl2 (image title)
-  (declare (ignore image title)))
+  (let* ((width (width image))
+         (height (height image))
+         (vector (array-storage-vector image))
+         (surface (sdl2:create-rgb-surface width height 24)) ;replace 24
+         (pixels (sdl2:surface-pixels surface)))
+    (dotimes (i (length vector))
+      (setf (cffi:mem-aref pixels :uint8 i) (aref vector i)))
+    (sdl2:with-init (:video)
+                    (sdl2:with-window (window :title title :w width :h height :flags '(:shown))
+                      (let ((screen-surface (sdl2:get-window-surface window)))
+                        (sdl2:blit-surface surface nil screen-surface nil)
+                        (sdl2:update-window window)
+                        (sdl2:delay 10000))))))
 
 
 
