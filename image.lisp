edd00aa96fafb67a974d6f4b394b9333005f8ed0
sparkecho <echozhz@126.com>
2018-06-04 23:55:56 +0800

    No need to omit alpha channel while reading, as jpg does not have

--- image.lisp
+++ image.lisp
@@ -3,17 +3,13 @@
 
 ;;;; Create image object (2d or 3d array)
 (defun make-image (rows cols channels data)
-  (let* ((dimensions (cond ((= channels 1) (list rows cols))
-                          ((>= channels 3) (list rows cols 3))))
+  (let* ((dimensions (if (= channels 1)
+                         (list rows cols)
+                         (list rows cols channels)))
          (image (make-array dimensions :element-type (array-element-type data)))
          (vector (array-storage-vector image)))
-    (if (<= channels 3)
-        (loop for i below (array-total-size image)
-           do (setf (aref vector i) (aref data i)))
-        (loop for i below (array-total-size image) by channels
-           do (setf (aref vector i) (aref data i)
-                    (aref vector (+ i 1)) (aref data (+ i 1))
-                    (aref vector (+ i 2)) (aref data (+ i 2)))))
+    (loop for i below (array-total-size image)
+       do (setf (aref vector i) (aref data i)))
     image))
 
 ;;;; Reture a vector (1-dimension array) which contains the given array's data
