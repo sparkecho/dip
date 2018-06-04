428fd999d85fc9cc988e08f93d748b553d1d63d9
sparkecho <echozhz@126.com>
2018-06-04 10:53:59 +0800

    Make var name more suitable.

--- dip.lisp
+++ dip.lisp
@@ -56,9 +56,9 @@
     (jpeg:encode-image filename (array-storage-vector image)
                        ncomp (height image) (width image))))
 
-(defparameter *gui-backend* :glfw3)
+(defparameter *default-backend* :glfw3)
 
-(defun imshow (image &optional (title "") (backend *gui-backend*))
+(defun imshow (image &optional (title "") (backend *default-backend*))
   (case backend
     (:glfw3 (imshow-glfw3 image title))
     (:sdl2 (imshow-sdl2 image title))))
