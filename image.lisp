ddc59e5624addb5197743ebb55c8c7588240ecc0
sparkecho <echozhz@126.com>
2018-06-05 16:57:38 +0800

    Use CLOS to make creating img more flexible

--- image.lisp
+++ image.lisp
@@ -1,14 +1,38 @@
 (in-package #:dip)
 
+;;;; Image creation
+(defgeneric make-image (rows cols channels data &optional element-type)
+  (:documentation "Create image object (2d or 3d array)."))
 
-;;;; Create image object (2d or 3d array)
-(defun make-image (rows cols channels data)
+(defmethod make-image (rows cols channels (data number)
+                       &optional (element-type '(unsigned-byte 8)))
+  (let ((dimensions (if (= channels 1)
+                        (list rows cols)
+                        (list rows cols channels))))
+    (make-array dimensions :element-type element-type :initial-element data)))
+
+(defmethod make-image (rows cols channels (data simple-vector)
+                       &optional (element-type '(unsigned-byte 8)))
+  (assert (= channels (length data)) nil
+          "Initial value's length does not match with image's channel number")
   (let* ((dimensions (if (= channels 1)
                          (list rows cols)
                          (list rows cols channels)))
-         (image (make-array dimensions :element-type (array-element-type data)))
+         (image (make-array dimensions :element-type element-type))
          (vector (array-storage-vector image)))
-    (loop for i below (array-total-size image)
+    (loop for i below (length vector) by channels
+       do (loop for j below channels
+             do (setf (aref vector (+ i j)) (svref data j))))
+    image))
+
+(defmethod make-image (rows cols channels (data array) &optional element-type)
+  (let* ((dimensions (if (= channels 1)
+                         (list rows cols)
+                         (list rows cols channels)))
+         (image (make-array dimensions
+                            :element-type (or element-type (array-element-type data))))
+         (vector (array-storage-vector image)))
+    (loop for i below (length data)
        do (setf (aref vector i) (aref data i)))
     image))
 
