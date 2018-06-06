0c3412bfe2567128efe9c30d4471ccd43a88e745
sparkecho <echozhz@126.com>
2018-06-06 15:00:41 +0800

    Add clx supportion

--- gui.lisp
+++ gui.lisp
@@ -1,8 +1,13 @@
 (in-package #:dip)
 
+;;;; After solved some clx issue (e.g. must have a rgb->bgra conversion now)
+;;;; And add support of win32-api
+;;;; Change definition of *default-backend* to:
+;;;; (defparameter *default-backend* #+unix :clx #+windows :win32 #+darwin :glfw3)
 (defparameter *default-backend* :glfw3)
 
 (defun imshow (image &optional (title "") (backend *default-backend*))
   (case backend
     (:glfw3 (imshow-glfw3 image title))
-    (:sdl2 (imshow-sdl2 image title))))
+    (:sdl2 (imshow-sdl2 image title))
+    (:clx (imshow-clx image title))))
