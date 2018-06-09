08eea2e45a757ff7f4200569caac11af800ead1f
sparkecho <echozhz@126.com>
2018-06-09 22:53:36 +0800

    Set sdl2 as default backend, it's best for now

--- gui.lisp
+++ gui.lisp
@@ -4,7 +4,7 @@
 ;;;; And add support of win32-api
 ;;;; Change definition of *default-backend* to:
 ;;;; (defparameter *default-backend* #+unix :clx #+windows :win32 #+darwin :glfw3)
-(defparameter *default-backend* :glfw3)
+(defparameter *default-backend* :sdl2)
 
 (defun imshow (image &optional (title "") (backend *default-backend*))
   (case backend
