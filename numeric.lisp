8afda0fe2abe524e894feba546a0bd8f810365e2
sparkecho <echozhz@126.com>
2018-08-06 23:50:21 +0800

    new function moved to here

--- numeric.lisp
+++ numeric.lisp
@@ -1,5 +1,17 @@
 (in-package #:dip)
 
+;; get the interval of a type
+;; e.g. interval of uint8 is [0 255]
+(declaim (inline type-interval))
+(defun type-interval (type)
+  #+sbcl
+  (let ((numeric-type (sb-kernel:specifier-type type)))
+    (values (sb-kernel:numeric-type-low numeric-type)
+            (sb-kernel:numeric-type-high numeric-type)))
+  #-sbcl
+  (error "only support sbcl right now"))
+
+
 (declaim (inline clamp-low clamp-high clamp))
 
 (defun clamp-low (number low)
