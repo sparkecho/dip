5d7e5c6a522adeed3a3515f78c3d094c4e155fe5
sparkecho <echozhz@126.com>
2018-08-10 01:15:24 +0800

    can calc feature for a pixel now

--- examples/hog.lisp
+++ examples/hog.lisp
@@ -39,6 +39,49 @@
                                          (atan (aref gy i j k) (aref gx i j k)))))))
     (values mag angle)))
 
+;; (defparameter table (let ((tb (vector 0 20 40 60 80 100 120 140 160)))
+;;                       (loop for i below 9
+;;                             do (setf (svref tb i)
+;;                                      (* pi (/ (svref tb i) 180))))
+;;                       tb))
+(defparameter table #(0 20 40 60 80 100 120 140 160))
+
+;; radian -> degree
+(defun radian-to-degree (radian)
+  (values (round (* 180 (/ radian pi)))))
+
+;; 0 <= degree < 180
+(defun degree-abs (angle)
+  (if (< angle 0)
+      (+ angle 180)
+      angle))
+
+;; radian image to degree image
+(defun rtd-image (radian-image)
+  (copy-value radian-image
+              (make-similar radian-image '(unsigned-byte 8))
+              #'(lambda (radian)
+                  (degree-abs (radian-to-degree radian)))))
+
+(defun find-pos (angle table)
+  (do ((i 0 (+ i 1)))
+      ((or (> i 8)
+           (> (aref table i) angle))
+       i)))
+
+;; calculate feature for a pixel
+;; mag, angle: (image (* *) uint8)
+(defun calc-feature (mag angle table feature)
+  (let* ((pos (find-pos angle table))
+         (l (- pos 1))
+         (r (mod pos 9))
+         (valuel (aref table l))
+         (ratior (/ (- angle valuel) 20))
+         (ratiol (- 1 ratior)))
+    (incf (aref feature l) (float (* mag ratiol)))
+    (incf (aref feature r) (float (* mag ratior)))
+    feature))
+
 (defparameter image (imread "../data/lena.jpg"))
 ;; Step 2 : Calculate the Gradient Images
 (defparameter fimage (convert-type image 'single-float))
@@ -49,5 +92,13 @@
 (multiple-value-bind (lmag langle)
     (cart-to-polar gx gy)
   (progn (setf mag lmag angle langle) t))
+
+(defparameter mag-uint8 (convert-type mag '(unsigned-byte 8)))
+(defparameter angle-uint8 (rtd-image angle))
+(imshow mag-uint8 "mag")
+(imshow angle-uint8 "angle")
+
+
+
 (destroy-all-windows)
 ;; Step 3 : Calculate Histogram of Gradients in 8×8 cells
